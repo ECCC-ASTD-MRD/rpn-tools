e8fef6b87905f8d11b90ab4886eb265972d7063c
Libraire <armnlib@ec.gc.ca>
2006-02-09 16:38:45 +0000

    Mis a jour le 22 decembre 2003 par Yves Chartier, pour AIX.

--- r.echo.c
+++ r.echo.c
@@ -13,14 +13,17 @@
 
      (par James Caveen, RPN/SI, 2 juillet 1993)
 
+     Mis a jour le 22 decembre 2003 par Yves Chartier, pour AIX.
+
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
      FILE *fichier;
-     char *ptr_echo_command_line, *ptr_diagnostik_exit;
+     unsigned char *ptr_echo_command_line, *ptr_diagnostik_exit;
 
 /*
  *   sauter le nom du programme
@@ -29,7 +32,7 @@ int main(int argc, char **argv)
 /*
  *   verifier l'environnement
  */
-       if ((ptr_diagnostik_exit = (char *)getenv("DIAGNOSTIC_EXIT")) == (char *) NULL) 
+       if ((ptr_diagnostik_exit = (unsigned char *)getenv("DIAGNOSTIC_EXIT")) == (unsigned char *) NULL) 
           ptr_diagnostik_exit="DIAGNOSTIC_EXIT";
 
        if(( fichier = fopen(ptr_diagnostik_exit,"r")) != (FILE *) NULL) 
@@ -39,7 +42,7 @@ int main(int argc, char **argv)
        }
           
        
-       if((ptr_echo_command_line = (char *)getenv("ECHO_COMMAND_LINE")) != (char *) NULL)
+       if((ptr_echo_command_line = (unsigned char *)getenv("ECHO_COMMAND_LINE")) != (unsigned char *) NULL)
        {     
        
            while (*argv)
